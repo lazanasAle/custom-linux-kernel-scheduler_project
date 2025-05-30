44bcf35bfbfd975f9485c46055beab578254ee05
lazanasAle <lazanas.ale@gmail.com>
2025-05-30 23:25:24 +0300

    Update testbench-syscalls.c

--- testbenches/testbench-syscalls.c
+++ testbenches/testbench-syscalls.c
@@ -12,6 +12,9 @@
 #define GOOD_TIME 1200
 #define BAD_TIME 120000
 
+#define set_sched_params(x, y, z) syscall(__NR_SET_SCHED_PARS, x, y, z)
+#define get_sched_params(x)       syscall(__NR_GET_SCHED_PARS, x)
+
 
 void print_params(struct d_params *params);
 void make_first_right();
@@ -57,45 +60,45 @@ void print_params(struct d_params *params){
 
 void make_first_right(){
     time_t now = time(NULL);
-    long ret1=syscall(__NR_SET_SCHED_PARS, now+7, now+12, GOOD_TIME);
+    long ret1=set_sched_params(now+7, now+12, GOOD_TIME);
     if(ret1!=0){
         printf("[-] incorrect rejection of parameters with values: D1: %ld D2: %ld CT: %ld\n", now+7, now+10, GOOD_TIME);
     }
     else{
         printf("[+] correctly accepted right parameters\n");
         struct d_params pars;
-        syscall(__NR_GET_SCHED_PARS, &pars);
+        get_sched_params(&pars);
         print_params(&pars);
     }
 }
 
 void make_first_wrong(){
     time_t now = time(NULL);
-    long ret1=syscall(__NR_SET_SCHED_PARS, now+11, now+8, GOOD_TIME);
+    long ret1=set_sched_params(now+11, now+8, GOOD_TIME);
     if(ret1!=0){
         printf("[+] correctly rejecting d2<d1 parameters\n");
     }
     else{
         printf("[-] the parameters were accepted incorrectly\n");
         struct d_params pars;
-        syscall(__NR_GET_SCHED_PARS, &pars);
+        get_sched_params(&pars);
         print_params(&pars);
     }
-    long ret2=syscall(__NR_SET_SCHED_PARS, now+7, now+13, BAD_TIME);
+    long ret2=set_sched_params(now+7, now+13, BAD_TIME);
     if(ret1!=0){
         printf("[+] correctly rejecting parameters with computation_time more than given in deadlines\n");
     }
     else{
         printf("[-] the parameters were accepted incorrectly\n");
         struct d_params pars;
-        syscall(__NR_GET_SCHED_PARS, &pars);
+        get_sched_params(&pars);
         print_params(&pars);
     }
 }
 
 void make_second_right(){
     struct d_params *pars=malloc(sizeof(struct d_params));
-    long ret_right1 = syscall(__NR_GET_SCHED_PARS, pars);
+    long ret_right1 = get_sched_params(pars);
     if(ret_right1!=0){
         printf("[-] incorrectly rejected a valid pointer\n");
     }
@@ -109,7 +112,7 @@ void make_second_right(){
 
 void make_second_wrong(){
     struct d_params *pars;
-    long ret_wrong1 = syscall(__NR_GET_SCHED_PARS, pars);
+    long ret_wrong1 = get_sched_params(pars);
     if(ret_wrong1!=0){
         printf("[+] correctly rejected an invalid pointer\n");
     }
@@ -118,7 +121,7 @@ void make_second_wrong(){
         print_params(pars);
     }
     pars=NULL;
-    ret_wrong1 = syscall(__NR_GET_SCHED_PARS, pars);
+    ret_wrong1 = get_sched_params(pars);
     if(ret_wrong1!=0){
         printf("[+] correctly rejected a NULL pointer\n");
     }
