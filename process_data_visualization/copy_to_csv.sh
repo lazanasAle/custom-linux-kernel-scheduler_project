5b14f2306f747793b8bce36640d31269c462b4ca
lazanasAle <lazanas.ale@gmail.com>
2025-06-29 11:51:49 +0300

    just changed csv contents

--- process_data_visualization/copy_to_csv.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-OUTPUT_FILE="hvf_log.csv"
-
-echo "pid,init_sched_value,turnaround_time,wait_time,computation_time,response_time" > "$OUTPUT_FILE"
-
-cat /sys/kernel/debug/tracing/trace_pipe | grep "hvf_task_terminated" | \
-    awk -F, '{ print $2","$3","$4","$5","$6 }' >> "$OUTPUT_FILE"
