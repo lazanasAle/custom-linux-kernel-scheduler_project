787b4314aa954efe2019857ae0a0bb8fb8ee6846
lazanasAle <lazanas.ale@gmail.com>
2025-07-01 09:20:39 +0300

    just changed the script

--- process_data_visualization/copy_to_csv.sh
+++ process_data_visualization/copy_to_csv.sh
@@ -5,4 +5,4 @@ OUTPUT_FILE="hvf_log.csv"
 echo "pid,init_sched_value,turnaround_time,wait_time,computation_time,response_time" > "$OUTPUT_FILE"
 
 cat /sys/kernel/debug/tracing/trace | grep "hvf_task_terminated" | \
-    awk -F, '{ print $2","$3","$4","$5","$6 }' >> "$OUTPUT_FILE"
+    awk -F, '{ print $2","$3","$4","$5","$6","$7 }' >> "$OUTPUT_FILE"
