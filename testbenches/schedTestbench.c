a01ecdf4566011264603009508e12e696eb87dfc
lazanasAle <lazanas.ale@gmail.com>
2025-10-25 15:07:25 +0300

    Update schedTestbench.c

--- testbenches/schedTestbench.c
+++ testbenches/schedTestbench.c
@@ -23,59 +23,54 @@
 
 double a = 1.1;
 
-void core_delay(){
-    unsigned long j;
-
-    for (j = 0; j < 100000; j++) {
-        a += sqrt(1.1)*sqrt(1.2)*sqrt(1.3)*sqrt(1.4)*sqrt(1.5);
-        a += sqrt(1.6)*sqrt(1.7)*sqrt(1.8)*sqrt(1.9)*sqrt(2.0);
-        a += sqrt(1.1)*sqrt(1.2)*sqrt(1.3)*sqrt(1.4)*sqrt(1.5);
-        a += sqrt(1.6)*sqrt(1.7)*sqrt(1.8)*sqrt(1.9);
-    }
-}
-
-void delay(int workload){
-    int i;
-    int total_workload = workload*DELAY;
-
-    for (i = 0; i < total_workload; i++)
-        core_delay();
-}
-
-void do_work(int workload){
-    int pid = getpid();
-
-    printf("process %d begins\n", pid);
-    delay(workload);
-    printf("process %d ends\n", pid);
-}
-
-int main(){
-    pid_t procs[20];
-
-    for (int j=0; j<20; ++j){
-        procs[j] = fork();
-        if (procs[j]<0){
-            printf("error in fork\n");
-            return 0;
+void core_delay() {
+        unsigned long j;
+        for (j = 0; j < 100000; j++) {
+                a += sqrt(1.1)*sqrt(1.2)*sqrt(1.3)*sqrt(1.4)*sqrt(1.5);
+                a += sqrt(1.6)*sqrt(1.7)*sqrt(1.8)*sqrt(1.9)*sqrt(2.0);
+                a += sqrt(1.1)*sqrt(1.2)*sqrt(1.3)*sqrt(1.4)*sqrt(1.5);
+                a += sqrt(1.6)*sqrt(1.7)*sqrt(1.8)*sqrt(1.9);
         }
-        else if (procs[j] == 0){
-            struct sched_param param = {.sched_priority=0};
-            long now = time(NULL);
-            if (!set_sched_params(now+j+1, now+j+7, K+H13*j)){
-                sched_setscheduler(0, SCHED_HVF, &param);
-                do_work(j+1);
-            }
-            else {
-                printf("invalid scheduling params\n");
-            }
-            exit(0);
+}
+
+void delay(int workload) {
+        int i;
+        int total_workload = workload*DELAY;
+        for (i = 0; i < total_workload; i++)
+                core_delay();
+}
+
+void do_work(int workload) {
+        int pid = getpid();
+        printf("process %d begins\n", pid);
+        delay(workload);
+        printf("process %d ends\n", pid);
+}
+
+int main() {
+        pid_t procs[20];
+        
+        for (int j = 0; j < 20; ++j) {
+                procs[j] = fork();
+                if (procs[j] < 0) {
+                        printf("error in fork\n");
+                        return 0;
+                }
+                else if (procs[j] == 0) {
+                        struct sched_param param = {.sched_priority=0};
+                        long now = time(NULL);
+                        if (!set_sched_params(now+j+1, now+j+7, K+H13*j)) {
+                                sched_setscheduler(0, SCHED_HVF, &param);
+                                do_work(j+1);
+                        }
+                        else {
+                                printf("invalid scheduling params\n");
+                        }
+                        exit(0);
+                }
         }
-    }
-
-    for (int j=0; j<20; ++j){
-        waitpid(procs[j], NULL, 0);
-    }
-
-    return 0;
+        for (int j = 0; j < 20; ++j) {
+                waitpid(procs[j], NULL, 0);
+        }
+        return 0;
 }
