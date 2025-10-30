f41c24e6d8f2fcb285325f3aec7cc88406d1f1fa
lazanasAle <lazanas.ale@gmail.com>
2025-10-30 20:36:22 +0200

    reverting back

--- customized-kernel/kernel/sched/hvf.c
+++ customized-kernel/kernel/sched/hvf.c
@@ -20,9 +20,6 @@
 #define H 100
 
 
-#define FROM false
-#define TO true
-
 long compute_init_sched_value(struct task_struct *p);
 long reduce_sched_value(struct sched_hvf_entity *se);
 void init_sched_hvf_entity(struct sched_hvf_entity *se);
@@ -141,35 +138,23 @@ dequeue_task_hvf(struct rq *rq, struct task_struct *p, int flags) {
 }
 
 static void
-context_switch_log(struct sched_hvf_entity *hvf, int pid, bool from_to) {
-	const char *setting = (from_to == FROM)? "from" : "to";
-	struct timespec64 now;
-	ktime_get_real_ts64(&now);
-	long curr_time = now.tv_sec*K + now.tv_nsec/(K*K);
-
-	pr_info("hvf_task_switched_%s,%d,%ld,%ld\n", setting, pid, hvf->curr_sched_value, curr_time);
-}
-
-static void
-set_next_hvf_entity(struct hvf_rq *hvf_rq, struct sched_hvf_entity *se, int pid) {
+set_next_hvf_entity(struct hvf_rq *hvf_rq, struct sched_hvf_entity *se) {
 	if (se->on_rq)
 		dequeue_hvf_entity(hvf_rq, se);
 
 	update_latest_se_hvf(se);
 
-	context_switch_log(se, pid, TO);
 	hvf_rq->curr = se;
 }
 
 
 static void set_next_task_hvf(struct rq *rq, struct task_struct *p, bool first) {
 	struct sched_hvf_entity *se_hvf = &p->hvf;
-	int pid = p->pid;
 	struct hvf_rq *hvf_rq = &rq->hvf;
 	if (!first)
 		return;
 
-	set_next_hvf_entity(hvf_rq, se_hvf, pid);
+	set_next_hvf_entity(hvf_rq, se_hvf);
 }
 
 
@@ -197,7 +182,6 @@ static void task_tick_hvf(struct rq *rq, struct task_struct *curr, int queued) {
 	curr_ent->runtime += TICK_NSEC;
 
         if (curr_ent->runtime >= slice*K*K) {
-		context_switch_log(curr_ent, curr->pid, FROM);
 		curr_ent->slice_expired = true;
 		resched_curr(rq);
 	}
--- process_data_visualization/copy_to_csv.sh
+++ process_data_visualization/copy_to_csv.sh
@@ -1,13 +1,8 @@
 #!/bin/sh
 
-OUTPUT_FILE1="times_hvf_log.csv"
-OUTPUT_FILE2="switches_hvf_log.csv"
+OUTPUT_FILE="hvf_log.csv"
 
-echo "pid,init_sched_value,turnaround_time,wait_time,computation_time,response_time" > "$OUTPUT_FILE1"
-echo "from_to,pid,curr_sched_value,time_event" > "$OUTPUT_FILE2"
+echo "pid,init_sched_value,turnaround_time,wait_time,computation_time,response_time" > "$OUTPUT_FILE"
 
 cat /sys/kernel/debug/tracing/trace | grep "hvf_task_terminated" | \
-    awk -F, '{ print $2","$3","$4","$5","$6","$7 }' >> "$OUTPUT_FILE1"
-
-dmesg | grep "hvf_task_switched_" | \
-    awk -F, '{ print $2","$3","$4","$5 }' >> "$OUTPUT_FILE2"
+    awk -F, '{ print $2","$3","$4","$5","$6","$7 }' >> "$OUTPUT_FILE"
