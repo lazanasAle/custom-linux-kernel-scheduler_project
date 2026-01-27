935a7669a3d6059f8e244e8f2890cd26dcc923b8
Alexis Lazanas <lazanas.ale@gmail.com>
2026-01-27 22:20:39 +0200

    added rust testing

---
+++ testbenches/testing_with_rust/src/main.rs
@@ -0,0 +1,9 @@
+mod syscall_bindings;
+
+const DELAY: u32 = 750;
+const K: u32 = 1000;
+const H13: u32 = 1300;
+
+fn main() {
+        println!("Here I am");
+}
---
+++ testbenches/testing_with_rust/src/syscall_bindings.rs
@@ -0,0 +1,42 @@
+use libc::{syscall, pid_t, sched_param, sched_setscheduler, fork};
+
+const SCHED_HVF: i32 = 8;
+const __NR_SET_SCHED_PARS: i64 = 467;
+const __NR_GET_SCHED_PARS: i64 = 468;
+const __NR_GET_SCHED_SCORE: i64 = 469;
+
+pub fn set_sched_params(x: i64, y: i64, z: i64) -> Result<(), std::io::Error> {
+        // safety note calling a custom syscall to set the parameters.
+        let ret: i64 = unsafe {
+                syscall(__NR_SET_SCHED_PARS, x, y, z)
+        };
+        if ret == -1 {
+                return Err(std::io::Error::last_os_error());
+        }
+        else {
+                return Ok(());
+        }
+}
+
+pub fn set_hvf_scheduler(pid: pid_t) {
+        let param = sched_param{sched_priority: 0};
+        // safety note calling syscall to change scheduler for this process.
+        unsafe {
+                sched_setscheduler(pid, SCHED_HVF, &param);
+        }
+}
+
+pub fn set_hvf_scheduler_current() {
+        set_hvf_scheduler(0);
+}
+
+pub fn safe_fork() -> Result<pid_t, std::io::Error> {
+        let pid: pid_t = unsafe { fork() };
+
+        if pid == -1 {
+                return Err(std::io::Error::last_os_error());
+        }
+        else {
+                return Ok(pid);
+        }
+}
